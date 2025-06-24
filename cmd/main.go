c3f412930fd9144363ec2d123337ce45bf8ec386
ArisuSak <120944868+ArisuSak@users.noreply.github.com>
2025-06-24 18:27:58 +0700

    chore: move type out of main file

--- cmd/main.go
+++ cmd/main.go
@@ -8,51 +8,10 @@ import (
 	"nats-jetstream/config"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/joho/godotenv"
-
-	"gopkg.in/yaml.v3"
 )
 
 
-var (
-    StreamService string
 
-	Subject      string
-	StreamName   string
-	ConsumerName string
-	DurableName  string
-	Url          string
-
-    app *config.ApplicationConfig
-)
-
-
-func init() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-
-    StreamService = os.Getenv("STREAMING_SERVICE")
-    Subject = os.Getenv("SUBJECT")
-    StreamName = os.Getenv("STREAM_NAME")
-    ConsumerName = os.Getenv("CONSUMER_NAME")
-    DurableName = os.Getenv("DURABLE_NAME")
-    Url = os.Getenv("NATS_URL")
-
-    data, err := os.ReadFile("config.yaml")
-    if err != nil {
-        log.Fatalf("Failed to read YAML file: %v", err)
-    }
-
-    var config config.ApplicationConfig
-    err = yaml.Unmarshal(data, &config)
-    if err != nil {
-        log.Fatalf("Failed to parse YAML file: %v", err)
-    }
-
-    app = &config
-}
 
 func main() {
     ctx := context.Background()
